a91552bca522ae18ca9e3429ab774833104f7759
jerrywu071 <jerrywu071@outlook.com>
2023-11-10 16:21:42 -0500

    changed save button to save icon
    need to change it to back button later

---
+++ app/src/main/java/com/example/swipenotes/GestureRecognitionActivity.java
@@ -0,0 +1,14 @@
+package com.example.swipenotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GestureRecognitionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gesture_recognition);
+    }
+}
